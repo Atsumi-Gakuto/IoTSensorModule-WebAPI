cf293bab071af4b0d99e9eb5375fd5b16a6157c5
Atsumi-Gakuto <b24702bb@aitech.ac.jp>
2025-12-04 18:36:18 +0900

    対応チェック関数で利用可能性もチェックするように変更

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,9 @@ export class IoTSensorModuleAPI {
 	 * クライアントがWeb Bluetoothに対応しているかどうかを取得し返す。
 	 * @returns 対応しているなら`true`、そうでないのなら`false`を返す。localhostからのアクセスやhttpsでのアクセスではない場合でも非対応扱いになるため接続は確認すること。
 	 */
-	public getIsSupportedWebBluetooth(): boolean {
-		return navigator.bluetooth != undefined;
+	public async getIsSupportedWebBluetooth(): Promise<boolean> {
+		if(navigator.bluetooth != undefined) return await navigator.bluetooth.getAvailability();
+		return false;
 	}
 }
 
