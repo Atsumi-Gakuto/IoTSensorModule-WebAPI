4384cbce94ba557a7871f698be75ee0356d45726
Atsumi-Gakuto <b24702bb@aitech.ac.jp>
2025-12-08 18:05:02 +0900

    コンソール出力を追加

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,20 @@ export class IoTSensorModuleAPI {
 	 * @returns 対応しているなら`true`、そうでないのなら`false`を返す。localhostからのアクセスやhttpsでのアクセスではない場合でも非対応扱いになるため接続は確認すること。
 	 */
 	public async getIsSupportedWebBluetooth(): Promise<boolean> {
-		if(navigator.bluetooth != undefined) return await navigator.bluetooth.getAvailability();
-		return false;
+		if(navigator.bluetooth != undefined) {
+			if(await navigator.bluetooth.getAvailability()) {
+				console.info(`Web Bluetooth is supported and available in this browser.`);
+				return true;
+			}
+			else {
+				console.warn('Web Bluetooth is supported but not available in this browser.');
+				return false;
+			}
+		}
+		else {
+			console.warn('Web Bluetooth is not supported in this browser.');
+			return false;
+		}
 	}
 }
 
